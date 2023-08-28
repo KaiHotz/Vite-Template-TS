487685e01465a3fa66fe0d74c396d390da5bfc63
KaiHotz <kaihotz@me.com>
2023-08-28 09:56:02 +0400

    eslint adjusted

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,74 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  root: true,
   extends: [
-    'react-app',
+    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
-  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', 'react-refresh', 'jsx-a11y', 'prettier'],
+  env: {
+    node: true,
+    browser: true,
+  },
+  settings: {
+    ecmaVersion: 'latest',
+    react: {
+      version: 'detect',
+    },
+  },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  ignorePatterns: ['!*.js', '.*.js', '*.json', 'scripts', 'src/graphql/generated/*', 'node_modules'],
   rules: {
-    'react/react-in-jsx-scope': 0,
+    'react-refresh/only-export-components': 'warn',
+    'newline-before-return': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-warning-comments': 'warn',
+    'object-shorthand': 'error',
+    'no-param-reassign': 'off',
+    'react/prop-types': 'off',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link', 'NextLink', 'RouterLink'],
+        aspects: ['invalidHref'],
+      },
+    ],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json', 'tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+        '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
+      },
+    },
+  ],
 };
