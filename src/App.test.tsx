2ff165e24faa683cc2183671ce1ff48318f23bd1
KaiHotz <kaihotz@me.com>
2023-08-28 09:59:38 +0400

    test adjustment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,11 +5,10 @@ import App from './App';
 
 describe('App', () => {
   it('Renders', () => {
-    // Arrange
     render(<App />);
-    // Act
-    // Expect
+
     const element = screen.getByRole('heading', { level: 1 });
+
     expect(element).toHaveTextContent('Vite + React');
   });
 });
